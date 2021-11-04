ac7cf2fec69a64f548aa240d02e1df1bdacb9f34
EziKielCroat <89482125+EziKielCroat@users.noreply.github.com>
2021-11-04 10:04:22 +0000

    Updated index_cisto.js

--- index_cisto.js
+++ index_cisto.js
@@ -1,34 +1,35 @@
 export let odgovor;
-const pitanja = ["Dobar dan", "Kako si", "Doviđenja"];
-const odgovori = ["Pozdrav", "Dobro, hvala!", "Do idućeg susreta"];
+const pitanja = ["dobar dan", "kako si", "doviđenja"];
+const odgovori = ["Pozdrav!", "Dobro, hvala!", "Do idućeg susreta"];
 
 document.addEventListener("DOMContentLoaded", () => {
-  const botun = document.getElementById("chat-submit");
-  botun.addEventListener("click", dohvatiPoruku);
+    const botun = document.getElementById("chat-submit");
+    botun.addEventListener("click", dohvatiPoruku);
 });
 
 function dohvatiPoruku() {
-  const poljeUnos = document.getElementById("unos");
-  let unos = poljeUnos.value;
-  rezultat(unos);
+    const poljeUnos = document.getElementById("unos");
+    let unos = poljeUnos.value;
+    let unos_pravi = unos.toLowerCase();
+    rezultat(unos_pravi);
 }
 
-function rezultat(unos) {
-  let brPitanja = usporedi(unos);
-  if (brPitanja > -1) {
-    odgovor = odgovori[brPitanja];
-  } else {
-    odgovor = "Ne razumijem pitanje";
-  }
+function rezultat(unos_pravi) {
+    let brPitanja = usporedi(unos_pravi);
+    if (brPitanja > -1) {
+        odgovor = odgovori[brPitanja];
+    } else {
+        odgovor = "Ne razumijem pitanje.";
+    }
 }
 
 function usporedi(ulaz) {
-  let brPitanja = -1;
-  for (let i = 0; i < pitanja.length; i++) {
-    if (ulaz == pitanja[i]) {
-      brPitanja = i;
-      break;
+    let brPitanja = -1;
+    for (let i = 0; i < pitanja.length; i++) {
+        if (ulaz == pitanja[i]) {
+            brPitanja = i;
+            break;
+        }
     }
-  }
-  return brPitanja;
+    return brPitanja;
 }
